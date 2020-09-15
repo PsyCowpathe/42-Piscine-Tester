20ec39517f9a6d767e22616fd4dc08d9897fe803
GauthierPoulain <poulain.gauthier16@gmail.com>
2020-09-15 11:36:26 +0200

    Update main01.c

--- c03/main01.c
+++ c03/main01.c
@@ -59,7 +59,8 @@ int		main(void)
 	char	s2[30] = "bonjour";
 	int		size = 0;
 
-	ft_putstr("Toi = ");
+	ft_putstr("#1");
+	ft_putstr("\nToi = ");
 	ft_putnbr(ft_strncmp(s1, s2, size));
 	ft_putstr("\nOfficiel = ");
 	ft_putnbr(strncmp(s1, s2, size));
@@ -68,7 +69,8 @@ int		main(void)
 	char	s4[30] = "bonjou";
 	int		size1 = 0;
 
-	ft_putstr("\n\nToi = ");
+	ft_putstr("\n#2");
+	ft_putstr("\nToi = ");
 	ft_putnbr(ft_strncmp(s3, s4, size1));
 	ft_putstr("\nOfficiel = ");
 	ft_putnbr(strncmp(s3, s4, size1));
@@ -77,7 +79,8 @@ int		main(void)
 	char	s6[30] = "";
 	int		size2 = 10;
 
-	ft_putstr("\n\nToi = ");
+	ft_putstr("\n#3");
+	ft_putstr("\nToi = ");
 	ft_putnbr(ft_strncmp(s5, s6, size2));
 	ft_putstr("\nOfficiel = ");
 	ft_putnbr(strncmp(s5, s6, size2));
@@ -86,7 +89,8 @@ int		main(void)
 	char	s8[30] = "dwdw";
 	int		size3 = 4;
 
-	ft_putstr("\n\nToi = ");
+	ft_putstr("\n#4");
+	ft_putstr("\nToi = ");
 	ft_putnbr(ft_strncmp(s7, s8, size3));
 	ft_putstr("\nOfficiel = ");
 	ft_putnbr(strncmp(s7, s8, size3));
@@ -96,7 +100,8 @@ int		main(void)
 	char	s10[30] = "dwdw";
 	int		size4 = 5;
 
-	ft_putstr("\n\nToi = ");
+	ft_putstr("\n#5");
+	ft_putstr("\nToi = ");
 	ft_putnbr(ft_strncmp(s9, s10, size4));
 	ft_putstr("\nOfficiel = ");
 	ft_putnbr(strncmp(s9, s10, size4));
@@ -105,7 +110,8 @@ int		main(void)
 	char	s12[30] = "rwdw";
 	int		size5 = 1;
 
-	ft_putstr("\n\nToi = ");
+	ft_putstr("\n#6");
+	ft_putstr("\nToi = ");
 	ft_putnbr(ft_strncmp(s11, s12, size5));
 	ft_putstr("\nOfficiel = ");
 	ft_putnbr(strncmp(s11, s12, size5));
@@ -114,7 +120,8 @@ int		main(void)
 	char	s14[30] = "dwdw";
 	int		size6 = 4;
 
-	ft_putstr("\n\nToi = ");
+	ft_putstr("\n#7");
+	ft_putstr("\nToi = ");
 	ft_putnbr(ft_strncmp(s13, s14, size6));
 	ft_putstr("\nOfficiel = ");
 	ft_putnbr(strncmp(s13, s14, size6));
@@ -123,7 +130,8 @@ int		main(void)
 	char	s16[30] = "dw  dw";
 	int		size7 = 5;
 
-	ft_putstr("\n\nToi = ");
+	ft_putstr("\n#8");
+	ft_putstr("\nToi = ");
 	ft_putnbr(ft_strncmp(s7, s8, size7));
 	ft_putstr("\nOfficiel = ");
 	ft_putnbr(strncmp(s15, s16, size7));
@@ -132,7 +140,8 @@ int		main(void)
 	char	s18[100] = "AAAAAAAAAAAAAbbbbbb123456789dwdwd\xF4 123456789";
 	int		size8 = 99;
 
-	ft_putstr("\n\nToi = ");
+	ft_putstr("\n#9");
+	ft_putstr("\nToi = ");
 	ft_putnbr(ft_strncmp(s7, s8, size8));
 	ft_putstr("\nOfficiel = ");
 	ft_putnbr(strncmp(s17, s18, size8));
@@ -140,7 +149,8 @@ int		main(void)
 	char	s19[] = "Helio";             //Merci Arpascal pour ces 3 test !
 	char	s20[] = "Hello";
 
-	ft_putstr("\n\nToi = ");
+	ft_putstr("\n#10");
+	ft_putstr("\nToi = ");
 	ft_putnbr(ft_strncmp(s19, s20, 5));
 	ft_putstr("\nOfficiel = ");
 	ft_putnbr(strncmp(s19, s20, 5));
