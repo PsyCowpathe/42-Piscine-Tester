59c7890c370f3ebfecd042843de7eaf396ca4b0c
GauthierPoulain <poulain.gauthier16@gmail.com>
2020-09-15 13:13:03 +0200

    Update main02.c

--- c03/main02.c
+++ c03/main02.c
@@ -38,7 +38,7 @@ int		main(void)
 	char	src1[11] = "coucou1234";
 
 	ft_putstr("\n\n#1");
-	ft_putstr("nToi = ");
+	ft_putstr("\nToi = ");
 	ft_putstr(ft_strcat(dest2, src1));
 	ft_putstr("\nOfficiel = ");
 	ft_putstr(strcat(dest3, src1));
