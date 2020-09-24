41d669817c31a975e2ee0f76fd825d1987ed40fd
Aurelien Girona <agirona@z3r2p3.42lyon.fr>
2020-09-24 10:11:55 +0200

    add test for ft_point.h

---
+++ c08/main03.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agirona <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/24 10:11:39 by agirona           #+#    #+#             */
+/*   Updated: 2020/09/24 10:11:42 by agirona          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_point.h"
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		ft_putchar(str[i++]);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putnbr(214748364);
+		ft_putchar(8 + 48);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(nb * -1);
+	}
+	else if (nb < 10)
+	{
+		ft_putchar(nb + 48);
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + 48);
+	}
+}
+
+void set_point(t_point *point)
+{
+	point->x = 42;
+	point->y = 21;
+}
+
+int main(void)
+{
+	t_point point;
+	set_point(&point);
+	ft_putstr("Toi      : X = ");
+	ft_putnbr(point.x);
+	ft_putstr(" Y = ");
+	ft_putnbr(point.y);
+	ft_putstr("\nOfficiel : X = 42 Y = 21\n");
+	return (0);
+}
